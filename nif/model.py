7116d4e21935de6a898b0f5c2edc8d640c71ff33
olivia <otthomas@uw.edu>
2022-04-25 10:43:03 -0700

    bug fix

--- nif/model.py
+++ nif/model.py
@@ -12,7 +12,7 @@ prune_low_magnitude = tfmot.sparsity.keras.prune_low_magnitude
 ConstantSparsity = tfmot.sparsity.keras.ConstantSparsity
 
 class NIF(Model):
-    def __init__(self, cfg_shape_net, cfg_parameter_net, pruning_params, prune_parameter_net = True, mixed_policy='float32'):
+    def __init__(self, cfg_shape_net, cfg_parameter_net, pruning_params,  mixed_policy='float32', prune_parameter_net = True):
         super(NIF, self).__init__()
         self.cfg_shape_net = cfg_shape_net
         self.si_dim = cfg_shape_net['input_dim']
